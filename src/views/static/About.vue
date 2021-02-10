208d68525fbe4ee1af5c57454f5f3d1b5c9ff85d
Pascal Wengerter <pascal@wengerter.info>
2021-02-10 14:31:34 +0000

    Add homescreen shortcut guide to about page

--- src/views/static/About.vue
+++ src/views/static/About.vue
@@ -4,6 +4,7 @@
     <WorkoutCard>
         <h1>About</h1>
         <p>In the era of tracking- and advertisement-heavy workout apps, this very basic solution provides an antithesis that you can use on any (modern) device with an up-to-date web browser.</p>
+        <p>Even though it's not published on the Android or iOS app store, you can add this app to your phone's home screen by following <a href="https://natomasunified.org/kb/add-website-to-mobile-device-home-screen/" target="_blank">this guide</a>.</p>
         <p>It had originally been created in early 2019 and since then was iterated a couple of times by <a href="https://pascal.rocks" target="_blank" class="inline-link">Pascal Wengerter</a> and friends.</p>
         <p>The term <strong><i>mate</i></strong> doesn't assume a gender - it's borrowed from the Australian slang for "buddy". Any individual is invited to use this application.</p>
         <p>The source code for this application is open source and can be found <a href="https://github.com/pascalwengerter/musclemate-frontend" target="_blank" class="inline-link">on GitHub</a>.</p>
