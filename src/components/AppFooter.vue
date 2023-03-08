2a26d4791a4fccb353dbef889a04610d951b6ced
Pascal Wengerter <pascal@wengerter.info>
2023-03-08 21:30:00 +0100

    Reintroduce footer by moving it into .vue component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="my-3 p-4 md:p-6">
+    <p>
+      <span class="md:float-left md:my-0 md:text-initial md:inline">
+        © 2023 MuscleMate
+      </span>
+      <span class="md:float-right md:my-0 md:text-initial md:inline lba">
+        Design by
+        <a href="https://lowbudgetagency.com" target="_blank" rel="noopener">
+          lowbudgetagency.com
+        </a>
+      </span>
+    </p>
+  </footer>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store/store";
-import WorkoutCard from "./components/WorkoutCard.vue";
+import AppFooter from "./components/AppFooter.vue";
 import BackButton from "./components/BackButton.vue";
 import DarkLogo from "./components/DarkLogo.vue";
+import WorkoutCard from "./components/WorkoutCard.vue";
 import "./index.css";
 
 const app = createApp(App).use(store).use(router);
-app.component("WorkoutCard", WorkoutCard);
-app.component("DarkLogo", DarkLogo);
+app.component("AppFooter", AppFooter);
 app.component("BackButton", BackButton);
+app.component("DarkLogo", DarkLogo);
+app.component("WorkoutCard", WorkoutCard);
 app.mount("#app");
