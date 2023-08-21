7448fd12df6d6930a682e245ff0b825589bdeaca
Pascal Wengerter <pascal@wengerter.info>
2023-08-21 21:20:14 +0200

    Fix paddings on Run View by making WorkoutCard innerPadding configurable

--- src/components/WorkoutCard.vue
+++ src/components/WorkoutCard.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+  <div class="grid grid-cols-2 md:grid-cols-4 gap-4 mt-5">
     <div
-      class="bg-white rounded-2xl workoutcard col-span-2 md:col-start-2 mx-8 py-7 px-5"
+      :class="`bg-white rounded-2xl workoutcard col-span-2 md:col-start-2 mx-8 ${innerPadding}`"
     >
       <slot />
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+withDefaults(defineProps<{ innerPadding: string }>(), {
+  innerPadding: "py-7 px-5",
+});
+</script>
